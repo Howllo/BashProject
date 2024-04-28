f09bc439e5ed807e364b2c473fb66560c92b7e4c
Tony Hardiman Jr <allenr64y@ymail.com>
2024-04-28 13:44:52 -0500

    Add reports and organized code.
    Added actual report to the files.Added name to the file name instead of it just being one large files with all the reports in it. Added a directory for easy cleaning the code location.

--- BashProject.sh
+++ BashProject.sh
@@ -1,8 +1,42 @@
 #!/bin/bash
 
-create_reports(){
-	names="RandomNames.txt"
+# Global Variables
+report=""
+name=""
+dir="Credit_Reports"
+
+if [ ! -d "$dir" ]; then
+	mkdir "$dir"
+fi
+
+# Create the files for the report with all needed information.
+create_report(){
 	paragraphs="RandomLatinParagraph.txt"
+
+	# Get random name.
+	ran_name=$((1 + RANDOM % (1000)))
+	name=$(sed -n "${ran_name}p" "$names")
+	
+	# Create User Credit Report
+	report="$name$credit_cards"
+	report="${report// /_}"
+	
+	# Create file.
+	touch "$report"
+	
+	# Get Random Report
+	random_report=$((1 + RANDOM % (5)))
+	temp_report="Credit_Report$random_report.txt"
+	
+	# Copy original into new.
+	cat $temp_report >> $report
+}
+
+# Generate the report with all the data, Credit Card number, 
+# expiration date, and security code.
+# Replace all the temp data in template with actual data.
+generate_report(){
+	names="RandomNames.txt"
 	credit_cards="CreditCard_Report.txt"
 	low=0
 	high=9
@@ -12,19 +46,23 @@ create_reports(){
 		rm "$credit_cards"
 	fi
 
-	touch "$credit_cards"
-
 	read -p "How many credit reports you want to generate: " total_credit
 	
 	while [ $count -lt $total_credit ]; do
 		# Reset the random number.
 		credit_num=""
 		
+		# Reset Report
+		report=""
+		
 		# Reset the Name
 		name=""
 		
 		# Reset the security number
 		security_num=""
+		
+		# Create the report file.
+		create_report
 
 		# Get Random credit card numbers. 5555 5555 5555 5555
 		for i in {1..16}; do
@@ -41,41 +79,43 @@ create_reports(){
 			security_num="$security_num$number"
 		done
 		
-		# Get random name.
-		ran_name=$((1 + RANDOM % (1000)))
-		name=$(sed -n "${ran_name}p" "$names")
+		# Get random date. 9/99
+		random_year=$((25 + RANDOM % (28-25+1)))
+		random_month=$((1 + RANDOM % (12)))
+		date="$random_month/$random_year"
 		
 		# Get random paragraph.
 		ran_para=$((1 + RANDOM % (15)))
 		para=$(sed -n "${ran_para}p" "$paragraphs")
 		
-		# Get random date.
-		random_year=$((25 + RANDOM % (28-25+1)))
-		random_month=$((1 + RANDOM % (12)))
-		date="$random_month/$random_year"
+		# Add Paragraph to report.
+		echo "$para" >> $report
 		
 		# Pattern match credit card number.
-		para=${para//'[credit card number]'/$credit_num}
+		sed -i "s/\[credit card number\]/$credit_num/g" "$report"
 		
 		# Pattern match security code.
-		para=${para//'[security code]'/$security_num}
+		sed -i "s/\[security code\]/$security_num/g" "$report"
 		
 		# Pattern match name.
-		para=${para//'[name]'/$name}
+		sed -i "s/\[name\]/$name/g" "$report"
 		
 		# Pattern match date.
-		para=${para//'[date]'/$date}
+		sed -i "s|\[date\]|$date|g" "$report"
 		
-		echo "$para" >> $credit_cards
+		# Move the finished report.
+		mv $report $dir
+		
 		((count++))
 	done
 }
+
 
 echo "Do you want to generate reports (y or n)?" 
 read -r user_input
 
 if [[ $user_input == "y" || $user_input == "yes" || $user_input == "Y" ]]; then
-	create_reports
+	generate_report
 fi
 
 exit 0 
